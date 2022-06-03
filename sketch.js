8c7a31a3634810bc27b25f0d829cb51c2b1bf156
ERR9Ros <cpieper205@gmail.com>
2022-06-03 22:06:59 +1000

    cleanup

--- sketch.js
+++ sketch.js
@@ -30,11 +30,13 @@ function windowResized() {
 
 function draw() {
   if (gameRun == true) {
-    let lvl = round(score / PI + 1);
-
-    if (lvl > enemies) {
+    let enemies = round(score / PI + 1);
+    if (enemies < 5) {
+      enemies = 5;
+    }
+    if (enemies > ghost.length) {
       ghost.push(new enemy());
-      enemies += 1;
+      enemies = ghost.length;
     }
 
     background(0);
@@ -61,7 +63,6 @@ function draw() {
           object.shift();
           object.push(new fruit());
           length += 10;
-          print("Trail:" + length);
           score += 1;
           if (score > highScore) {
             storeItem("highScore", score);
@@ -79,13 +80,13 @@ function draw() {
     //UI stuffs
     rectMode(CORNER);
     fill(255);
-    rect(0, -10, width*2, 25);
+    rect(0, -10, width * 2, 25);
     fill(0);
     textSize(15);
     textAlign(LEFT, TOP);
-    text("Score:" + score, 0, 0);
-    text("HighScore:" + highScore, width * 0.75, 0);
-    text("Health:" + length, width / 2, 0);
+    text("Score: " + score, 0, 0);
+    text("HighScore: " + highScore, width * 0.75, 0);
+    text("Health: " + length, width / 2, 0);
     text("Ghosts: " + enemies, width * 0.2, 0);
     if (length == 0) {
       rectMode(CENTER);
@@ -93,8 +94,8 @@ function draw() {
       fill(255);
       rect(width / 2, height / 2, 210, 210);
       fill(0);
-      text("Score:" + score, width / 2 - 70, height / 2);
-      text("HighScore:" + highScore, width / 2 - 70, height / 2 + 20);
+      text("Score: " + score, width / 2 - 70, height / 2);
+      text("HighScore: " + highScore, width / 2 - 70, height / 2 + 20);
       text("Hit SPACE to play again", width / 2 - 80, height / 2 + 50);
       textSize(20);
       text("GAME OVER!", width / 2 - 70, height / 2 - 25);
@@ -103,13 +104,15 @@ function draw() {
       highScore += 1;
       storeItem("highScore", score);
     }
-  } else if (gameRun == false) {
-    background(0, 0, 120);
-    for (let b of startButton) {
-      b.display();
+    if (gameRun == false) {
+      background(0, 0, 120);
+      for (let b of startButton) {
+        b.display();
+      }
     }
   }
 }
+
 function keyPressed() {
   for (let p of player) {
     p.control();
@@ -135,6 +138,7 @@ function keyPressed() {
     ghostPause = true;
   }
 }
+
 function mouseClicked() {
   for (let b of startButton) {
     b.click();
