f674f3fc24811dc5d8cdfb43a1279036ad7a74f4
ERR9Ros <88700442+ERR9Ros@users.noreply.github.com>
2022-06-08 08:57:22 +1000

    Formatting

--- enemy.js
+++ enemy.js
@@ -2,7 +2,10 @@ class enemy {
   constructor() {
     this.r = 7;
     this.width = this.r * 2;
-    this.pos = createVector(random(this.r, width - this.r), random(20 + this.r, height - this.r));
+    this.pos = createVector(
+      random(this.r, width - this.r),
+      random(20 + this.r, height - this.r)
+    );
     this.vel = createVector(0, 0);
     this.speed = random(0.4, 0.9);
     this.pause = createVector(0, 0);
@@ -37,8 +40,14 @@ class enemy {
   }
   intersects(other) {
     for (var i = 0; i < ghost.length; i++) {
-      if (this.pos != ghost[i].pos && this.pos.dist(ghost[i].pos) < this.width) {
-        this.vel = createVector(ghost[i].pos.x - this.pos.x, ghost[i].pos.y - this.pos.y);
+      if (
+        this.pos != ghost[i].pos &&
+        this.pos.dist(ghost[i].pos) < this.width
+      ) {
+        this.vel = createVector(
+          ghost[i].pos.x - this.pos.x,
+          ghost[i].pos.y - this.pos.y
+        );
         ghost[i].vel.limit(ghost[i].speed);
         ghost[i].pos.sub(ghost[i].vel);
       }
